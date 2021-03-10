d8ad2879c18f031af7837255f0ba7bc805e129de
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-03-10 13:31:58 +0100

    fix header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,45 @@
+import React from "react";
+import {
+  BusinessCenter,
+  SupervisorAccount,
+  Home,
+  Search,
+  Chat,
+  Notifications,
+} from "@material-ui/icons";
+import HeaderOption from "./HeaderOption";
+import "./Header.scss";
 
-import React from 'react'
-import './Header.css'
-
+/**
+ * Header Component is the navbar for our website
+ */
 function Header() {
-    return (
-        <div className="header">
-            <h1>This is the header</h1>
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt="linkedIn-icon"
+        />
+        <div className="header__left__search">
+          <Search />
+          <input type="text" />
         </div>
-    )
+      </div>
+
+      <div className="header__right">
+        <HeaderOption Icon={Home} title="Home" />
+        <HeaderOption Icon={SupervisorAccount} title="My Network" />
+        <HeaderOption Icon={BusinessCenter} title="Jobs" />
+        <HeaderOption Icon={Chat} title="Messaging" />
+        <HeaderOption Icon={Notifications} title="Notification" />
+        <HeaderOption
+          avatar="https://media-exp1.licdn.com/dms/image/C4D35AQHO-61aPlMFpA/profile-framedphoto-shrink_200_200/0/1614880986043?e=1615467600&v=beta&t=3Kt_tP236o2ah1mqIEEGT6UJSgVKsUAxPC_XEda8fPk"
+          title="me"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
