b3233f1df75781ff907f70a1cda219a2630ed135
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-03-13 23:15:47 +0100

    add posts

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Feed.scss";
 import InputOption from "./InputOption";
 import {
@@ -8,18 +8,29 @@ import {
   Image,
   Subscriptions,
 } from "@material-ui/icons";
+import Post from "./Post";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+  const sendPost = (event) => {
+    event.preventDefault(); // stop refresh the page when click enter (Send btn)
+    setPosts([...posts]);
+  };
   return (
     <div className="feed">
+      {/* Create Post Form */}
       <div className="feed__inputContainer">
+        {/* Input */}
         <div className="feed__inputContainer__input">
           <Create />
           <form>
             <input type="text" />
-            <button type="submit">Send</button>
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
+        {/* Input Option Photo, Video, Event... */}
         <div className="feed__inputContainer__inputOptions">
           <InputOption Icon={Image} title="Photo" color="#70B5F9" />
           <InputOption Icon={Subscriptions} title="Video" color="#E7A33E" />
@@ -31,6 +42,15 @@ function Feed() {
           />
         </div>
       </div>
+      {/*  Posts  */}
+      {posts.map((post) => (
+        <Post
+          name="name"
+          decription="decription"
+          message=" sdqsdsqd sdqsd qdqsdsq sdqs sdqsdsqd sqdsq dsqd sqd qsd qge"
+          photoUrl="photoUrl"
+        />
+      ))}
     </div>
   );
 }
---
+++ src/Components/Feed/Post.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Post.scss";
+import { Avatar } from "@material-ui/core";
+import {
+  ChatOutlined,
+  SendOutlined,
+  ShareOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
+import InputOption from "./InputOption";
+
+function Post({ name, decription, message, photoUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar />
+        <div className="post__header__info">
+          <h2>{name}</h2>
+          <p>{decription}</p>
+        </div>
+      </div>
+
+      <div className="post__body">
+        <p>{message}</p>
+      </div>
+
+      <div className="post__buttons">
+        <InputOption Icon={ThumbUpAltOutlined} title="Like" color="gray" />
+        <InputOption Icon={ChatOutlined} title="Comment" color="gray" />
+        <InputOption Icon={ShareOutlined} title="Share" color="gray" />
+        <InputOption Icon={SendOutlined} title="Send" color="gray" />
+      </div>
+    </div>
+  );
+}
+
+export default Post;
